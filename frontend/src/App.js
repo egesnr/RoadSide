d355b1153b31d069557cc9872e79d54f191c73ef
Ege <egesnr1@gmail.com>
2023-08-25 05:14:03 +0300

    adding tab

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
 import useGeoLocation from './Components/GeoLocation';
 import AlignItemsList from './Components/ServiceList';
+import BasicTabs from './Components/Tab';
 
 function App() {
   const location = useGeoLocation();
+
+  if (location.error) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>ROADSIDE</h1>
+          <p>Geolocation Error: {location.error.message}</p>
+        </header>
+      </div>
+    );
+  }
+
+  if (!location.loaded) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>ROADSIDE</h1>
+          <p>Loading geolocation...</p>
+        </header>
+      </div>
+    );
+  }
 
   return (
     <div className="App">
@@ -11,10 +34,12 @@ function App() {
         <h1>ROADSIDE</h1>
         <p>Welcome to roadside assistance service!!!!</p>
         <AlignItemsList />
+        <BasicTabs />
       </header>
-      
     </div>
   );
 }
+
+
 
 export default App;
